dbe6a8b211618802a772dd9ea00eb6e5c87dd251
Sle2p <53994097+Sle2p@users.noreply.github.com>
2022-07-28 23:57:57 +0800

    update

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const Koa = require('koa')
 
 const app = new Koa
 
+const PORT = process.env.PORT || 3001
+
 app.use(async ctx => {
-  ctx.body = ctx.request.url
+  ctx.body = '1'
 })
 
-app.listen(process.env.PORT || 3333)
+app.listen(PORT)
